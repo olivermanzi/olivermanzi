8a11bf37b5edd668ad0f2bef24fca4767da13989
olivermanzi <olivermanzi.dev@gmail.com>
2021-03-20 14:20:14 +0100

    adds a bio card to the landing page

---
+++ frontend/src/components/BioCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <b-row
+      align-h="center">
+      <b-col
+        cols="auto">
+        <b-button-group>
+          <b-button 
+            class="mx-1"
+            size="sm"
+            variant="outline-primary"
+            @click="shortBio = true">
+            {{ $t("landingPage.actions.short") }}
+          </b-button>
+          <b-button 
+            class="mx-1"
+            size="sm"
+            variant="outline-danger"
+            @click="shortBio = false">
+            {{ $t("landingPage.actions.long") }}
+          </b-button>
+        </b-button-group>
+      </b-col>
+    </b-row>
+    <b-row
+      class="mt-3"
+      align-h="center">
+      <b-col
+        sm="11"
+        md="10">
+        <div
+          class="bio-text"
+          v-html="getMarkdown" />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+	import { getMarkdown } from "../helpers/markdown-helper";
+
+	export default {
+		name:"BioCard",
+		props:{
+			short:{
+				type: String,
+				required: true
+			},
+			long:{
+				type: String,
+				required: true
+			},
+		},
+		data: function(){
+			return {
+				shortBio: true,
+			};
+		},
+		computed:{
+			getMarkdown: function(){
+				return this.shortBio ? getMarkdown(this.short) : getMarkdown(this.long);
+			}
+		},
+
+	};
+</script>
